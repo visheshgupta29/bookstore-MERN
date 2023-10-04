0cebf83ffa380e6d64c91e28bc71e6c17b672027
Vishesh Gupta <73523718+visheshgupta29@users.noreply.github.com>
2023-10-04 22:27:35 +0530

    frontend axios+home

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,24 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoutes.js";
+import cors from "cors";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with  Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: "https://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (req, res) => {
   // console.log(req);
