65ae75b7919902aa7666a30670898db53f3f7b7d
Vishesh Gupta <73523718+visheshgupta29@users.noreply.github.com>
2023-10-07 03:40:06 +0530

    frontend notistack alert

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,23 +3,28 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleDeleteBook = () => {
     setLoading(true);
     axios
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book deleted successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
-        console.log(error);
-        alert("An error occurred. Please check console.");
         setLoading(false);
+        // alert("An error occurred. Please check console.");
+        enqueueSnackbar("Error", { variant: "error" });
+        console.log(error);
       });
   };
   return (
